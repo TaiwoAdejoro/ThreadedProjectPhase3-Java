1c0fb6647bb6463f1c434a63a8bd10d361e84826
jjjjjking <jonbradsmith@gmail.com>
2025-02-22 15:39:52 -0700

    created connection to Travel Experts DB

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.travelexpertsphase3desktop {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.travelexpertsphase3desktop to javafx.fxml;
---
+++ src/main/java/org/example/travelexpertsphase3desktop/DBconnection.java
@@ -0,0 +1,45 @@
+package org.example.travelexpertsphase3desktop;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class DBconnection {
+    public static Properties loadProperties() {
+        Properties prop = new Properties();
+
+        try (InputStream in = DBconnection.class.getClassLoader().getResourceAsStream("DBconfig")) {
+            if (in == null) {
+                System.out.println("Sorry, there was an error loading the Config file");
+                return null;
+            }
+            prop.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    public static Connection getConnection() {
+        Properties prop = DBconnection.loadProperties();
+        if (prop == null) {
+            System.out.println("Sorry, there was an error loading the Config file");
+            return null;
+        }
+
+        String url = prop.getProperty("URL");
+        String user = prop.getProperty("USER");
+        String password = prop.getProperty("PASS");
+
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
